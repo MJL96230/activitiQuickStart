e8c84bd3c3278d9be23fce3434e89b31184566bd
mao <mjl96230@163.com>
2019-11-27 16:18:06 +0800

    first commit

---
+++ src/main/java/com/mao/activitiquickstart/ActivitiQuickstartApplication.java
@@ -0,0 +1,13 @@
+package com.mao.activitiquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiQuickstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiQuickstartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mao/activitiquickstart/ActivitiQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mao.activitiquickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitiQuickstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
